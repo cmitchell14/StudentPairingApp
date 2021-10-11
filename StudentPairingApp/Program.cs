cff5db8f5d8facdbe4d62b8b183e63bb6d800309
cmitchell14 <calmitch14@gmail.com>
2021-10-11 13:29:40 -0500

    Initial Commit

--- StudentPairingApp/Program.cs
+++ StudentPairingApp/Program.cs
@@ -10,6 +10,45 @@ namespace StudentPairingApp
     {
         static void Main(string[] args)
         {
+            List<string> students = new List<string>() { };
+            string student;
+            bool exit = false;
+
+
+            do
+            {
+
+                Console.Write("Please type a student's name to add them to the list: ");
+                student = Console.ReadLine();
+                students.Add(student);
+                Console.WriteLine($"{student} has been added to the list\n\nWould you like to add another student?  Y/N");
+
+                ConsoleKey choice = Console.ReadKey().Key;
+                bool exit2 = false;
+                while (!exit2)
+                {
+                    switch (choice)
+                    {
+                        case ConsoleKey.Y:
+                            exit2 = true;
+                            break;
+                        case ConsoleKey.N:
+                            exit = true;
+                            exit2 = true;
+                            break;
+                        default:
+                            Console.WriteLine("Invalid option.  Please choose again.");
+                            break;
+                    }
+
+                }
+
+            } while (!exit);
+
+
+           
+
+
         }
     }
 }
